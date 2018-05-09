06823a1b37b56dd0f197846fe39faf94dabbc75a
Rachel Elam <relam24@gmail.com>
2018-05-09 14:10:37 -0400

    added header

--- models/projects.js
+++ models/projects.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const projectsSchema = new mongoose.Schema({
 	name: {type: String},
 	img: {type: String},
-	url: {type: String}
+	url: {type: String},
+	description: {type: String}
 });
 
 const Projects = mongoose.model('Projects', projectsSchema);
